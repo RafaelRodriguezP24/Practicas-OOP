f0b30de5fb6a05b6ed5493ff2694622bc7d2289c
Eltukyrafa <rafaelalejandrop2001@gmail.com>
2024-08-13 09:00:51 -0400

    segundo comiit de las practicas

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
 
-$firsname = "John";
-$lastname = "Doe";
+final class Person {
+    public $firstname;
+    public $lastname;
 
-echo $firstname . " " . $lastname;
+    public function __construct
+    (
+        $firstname,
+        $lastname
+    )
+    {
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+    }
+
+    public function fullName()
+    {
+        return $this->firstname . " " . $this->lastname;
+    }
+}
+
+$person1 = new Person('John', 'Doe');
+$person2 = new Person('Jane', 'Doe');
+
+
+echo "{$person1->fullName()} es amigo de {$person2->fullName()}";
+
 
