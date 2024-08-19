7db4c740fa131e9b61af0f4910ff25370b068d1f
Eltukyrafa <rafaelalejandrop2001@gmail.com>
2024-08-19 09:31:02 -0400

    Practicas OOP video 5 Interfaces y polimorfismo

--- inheritance.php
+++ inheritance.php
@@ -2,8 +2,7 @@
 abstract class Unit{
     protected string $name;
     protected int $hp = 100;
-
-    public function __construct($name){
+    public function __construct($name, Armor $armor = null){
         $this->name = $name;
     }
     public function move(): void
@@ -23,6 +22,12 @@ abstract class Unit{
     }
     public function takeDamage($damage): int
     {
+        if($this->armor != null){
+            $damage = $this->armor->reduceDamage($damage);
+            if ($damage == 0) {
+                show("{$this->name} ha logrado esquivar el ataque");
+            }
+        }
         $this->hp -= $damage;
 
         show("{$this->name} ha recibido {$damage} puntos de daño");
@@ -41,74 +46,83 @@ abstract class Unit{
     private function die(): void
     {
         show("{$this->name} ha muerto");
+        exit();
     }
+
+    public function reduceDamage($damage): int
+    {
+        return $damage;
+    }
+
+    public function setArmor(Armor $armor = null): void
+    {
+        $this->armor = $armor;
+    }
+
 }
 class Soldier extends unit{
     protected int $damage = 40;
-    protected string $armor;
-
-    const  ARMOR_BLACK = 'black';
-    const  ARMOR_SILVER = 'silver';
-    const  ARMOR_GOLD = 'gold';
-
-    /**
-     * @throws Exception
-     */
-    public function __construct($name, $armor){
+    protected Armor $armor;
+    public function __construct($name, Armor $armor = null){
         parent::__construct($name);
-        if (!in_array($armor, [
-            self::ARMOR_BLACK,
-            self::ARMOR_SILVER,
-            self::ARMOR_GOLD
-        ])) {
-            throw new Exception("Armor type not valid");
-        }
-        $this->armor = $armor;
+        parent::setArmor($armor);
     }
+
     public function attack(unit $opponent): void
     {
         show("{$this->name} ataca con la espada a {$opponent->getName()}");
 
         $opponent->takeDamage($this->damage);
     }
-    public function takeDamage($damage): int
-    {
-        $damage = $this->reduceDamage($damage);
-        return parent::takeDamage($damage);
-    }
-
-    private function reduceDamage($damage): int
-    {
-        if ($this->armor === self::ARMOR_BLACK) {
-            return $damage / 4;
-        }
-        if ($this->armor === self::ARMOR_SILVER) {
-            return $damage / 2;
-        }
-        if ($this->armor === self::ARMOR_GOLD){
-            return $damage / 3;
-        }
-        return $damage;
-    }
-
 }
-
+interface Armor
+{
+    public function reduceDamage($damage): int;
+}
+class SilverArmor implements Armor
+{
+    public function reduceDamage($damage): int
+    {
+        return $damage / 2;
+    }
+}
+class GoldArmor implements Armor
+{
+    public function reduceDamage($damage): int
+    {
+        return $damage / 3;
+    }
+}
+class BlackArmor implements Armor
+{
+    public function reduceDamage($damage): int
+    {
+        return $damage / 4;
+    }
+}
+class ArcherArmor implements Armor
+{
+    public function reduceDamage($damage): int
+    {
+        if (rand(0, 1)) {
+            return $damage;
+        }else{
+            return $damage = 0;
+        }
+    }
+}
 class Archer extends unit{
     protected int $damage = 20;
+    protected Armor $armor;
+    public function __construct($name, Armor $armor = null){
+        parent::__construct($name);
+        parent::setArmor($armor);
+    }
     public function attack(unit $opponent): void
     {
         show("{$this->name} dispara una flecha a {$opponent->getName()}");
 
         $opponent->takeDamage($this->damage);
-    }
-    public function takeDamage($damage): int
-    {
-        if (rand(0, 1)) {
-            return parent::takeDamage($damage);
-        } else {
-            show("{$this->name} ha logrado esquivar el ataque");
-            return parent::takeDamage($damage = 0);
-        }
     }
 }
 
@@ -117,14 +131,16 @@ function show($message): void
     echo "<p>$message</p>";
 }
 
-$robin = new Archer('Robin Hood');
-$arturo = new Soldier('Arturo', 'black');
-$rafael = new Soldier('Rafael', 'silver');
-$juan = new Soldier('Juan', 'gold');
+$armor = new SilverArmor();
+$armorArc = new ArcherArmor();
+
+$robin = new Archer('Robin Hood', $armorArc);
+$arturo = new Soldier('Arturo', $armor);
 
 $robin->attack($arturo);
 $arturo->attack($robin);
-$rafael->attack($robin);
-$juan->attack($rafael);
-$robin->attack($rafael);
-$arturo->attack($juan);
+$arturo->attack($robin);
+$arturo->attack($robin);
+$arturo->attack($robin);
+
+
