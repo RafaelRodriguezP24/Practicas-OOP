4d50ead4b51f6d93190096c1b10c58f9151eb418
Eltukyrafa <rafaelalejandrop2001@gmail.com>
2024-08-18 22:46:22 -0400

    Practicas OOP video 4 Interacciones entre objetos

--- inheritance.php
+++ inheritance.php
@@ -1,36 +1,130 @@
 <?php
-
 abstract class Unit{
-    public $name;
-    public $alive = true;
-    public $weapon;
+    protected string $name;
+    protected int $hp = 100;
 
-    public function __construct($name, $weapon){
+    public function __construct($name){
         $this->name = $name;
-        $this->weapon = $weapon;
     }
-
-    public function move(){
+    public function move(): void
+    {
         echo $this->name . ' is moving to the battlefield ';
     }
+    public  function attack(unit $opponent){
 
-    abstract  function attack();
-
-}
-
-class Soldier extends unit{
-    public function attack(){
-        echo $this->name . ' cut with ' . $this->weapon;
     }
+    public function getHp(): int
+    {
+        return $this->hp;
+    }
+    public function setHp(int $hp): void
+    {
+        $this->hp = $hp;
+    }
+    public function takeDamage($damage): int
+    {
+        $this->hp -= $damage;
+
+        show("{$this->name} ha recibido {$damage} puntos de daño");
+
+        if($this->hp <= 0){
+            $this->die();
+        }else{
+            show("{$this->name} tiene {$this->hp} puntos de vida");
+        }
+        return $this->hp;
+    }
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    private function die(): void
+    {
+        show("{$this->name} ha muerto");
+    }
+}
+class Soldier extends unit{
+    protected int $damage = 40;
+    protected string $armor;
+
+    const  ARMOR_BLACK = 'black';
+    const  ARMOR_SILVER = 'silver';
+    const  ARMOR_GOLD = 'gold';
+
+    /**
+     * @throws Exception
+     */
+    public function __construct($name, $armor){
+        parent::__construct($name);
+        if (!in_array($armor, [
+            self::ARMOR_BLACK,
+            self::ARMOR_SILVER,
+            self::ARMOR_GOLD
+        ])) {
+            throw new Exception("Armor type not valid");
+        }
+        $this->armor = $armor;
+    }
+    public function attack(unit $opponent): void
+    {
+        show("{$this->name} ataca con la espada a {$opponent->getName()}");
+
+        $opponent->takeDamage($this->damage);
+    }
+    public function takeDamage($damage): int
+    {
+        $damage = $this->reduceDamage($damage);
+        return parent::takeDamage($damage);
+    }
+
+    private function reduceDamage($damage): int
+    {
+        if ($this->armor === self::ARMOR_BLACK) {
+            return $damage / 4;
+        }
+        if ($this->armor === self::ARMOR_SILVER) {
+            return $damage / 2;
+        }
+        if ($this->armor === self::ARMOR_GOLD){
+            return $damage / 3;
+        }
+        return $damage;
+    }
+
 }
 
 class Archer extends unit{
-    public function attack(){
-        echo $this->name . ' is attacking with ' . $this->weapon;
+    protected int $damage = 20;
+    public function attack(unit $opponent): void
+    {
+        show("{$this->name} dispara una flecha a {$opponent->getName()}");
+
+        $opponent->takeDamage($this->damage);
+    }
+    public function takeDamage($damage): int
+    {
+        if (rand(0, 1)) {
+            return parent::takeDamage($damage);
+        } else {
+            show("{$this->name} ha logrado esquivar el ataque");
+            return parent::takeDamage($damage = 0);
+        }
     }
 }
 
-$archer = new Archer('archer', 'bow');
-$soldier = new Soldier('soldier', 'sword');
+function show($message): void
+{
+    echo "<p>$message</p>";
+}
 
-echo "<p>{$soldier->move()} {$archer->attack()}</p>";
+$robin = new Archer('Robin Hood');
+$arturo = new Soldier('Arturo', 'black');
+$rafael = new Soldier('Rafael', 'silver');
+$juan = new Soldier('Juan', 'gold');
+
+$robin->attack($arturo);
+$arturo->attack($robin);
+$rafael->attack($robin);
+$juan->attack($rafael);
+$robin->attack($rafael);
+$arturo->attack($juan);
