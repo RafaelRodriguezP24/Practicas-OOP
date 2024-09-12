03acfe190c057c423e84c6be0536759c26e03725
Eltukyrafa <rafaelalejandrop2001@gmail.com>
2024-09-12 09:37:57 -0400

    Practicas OOP video 15 Metodos Magicos

---
+++ Public/magicalMethods.php
@@ -0,0 +1,25 @@
+<?php
+namespace Medine;
+
+use Medine\User;
+
+require '../vendor/autoload.php';
+
+$user = new User();
+
+$user->setAttributes([
+    'first_name' => 'John',
+    'last_name' => 'Doe',
+]);
+
+$user->nickname = 'johndoe';
+
+unset($user->nickname);
+
+echo "<p>Bienvenido {$user->first_name} {$user->last_name}</p>";
+
+if (isset($user->nickname)) {
+    echo "<p>Tu apodo es {$user->nickname}</p>";
+} else {
+    echo "<p>No tienes apodo</p>";
+}
